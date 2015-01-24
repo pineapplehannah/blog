09dfe6f64b214536f5ed2161e9f72a1f82f328fa
Hannah Stephenson <littlehannah96@gmail.com>
2015-01-24 22:15:20 +0000

    Renamed myapp.rb to app.rb, and refactored code into Blog class. Created config.ru file

---
+++ app.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'sinatra'
+
+#get '/' do
+#	"I'm a Home Page and I will display 4 posts!"	
+#end
+#
+#get '/about' do
+#	"Here will be some information about me"
+#end
+#
+#get '/blog/:post' do
+#	"My blog post will be here"
+#end
+
+class Blog < Sinatra::Base
+	get '/' do
+		"I'm a Home Page and I will display 4 posts!"	
+	end
+
+	get '/about' do
+		"Here will be some information about me"
+	end
+
+	get '/blog/:post' do
+		"My blog post will be here"
+	end
+end
